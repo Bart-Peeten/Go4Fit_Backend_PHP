5878c7effa3e8a35e691a3221ebb39f10e8f5415
Lenny Kraaijenhof <hi@lenny.kr>
2020-02-06 13:25:11 +0100

    Create basic JSON response for common HTTP errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,24 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'error' => [
+                    'code' => 404,
+                    'message' => "Not found",
+                ]
+            ], 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'error' => [
+                    'code' => 405,
+                    'message' => "Method is not supported for this route",
+                ]
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
