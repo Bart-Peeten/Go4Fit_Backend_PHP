1628664597fa197826cd68125f5ce1eef5006d8f
Bart-Peeten <bart.peeten@gmail.com>
2020-03-03 21:58:39 +0100

    Add new migration for deleted_users

---
+++ database/migrations/2020_03_03_192515_create_deleted_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeletedReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deleted_reservations', function (Blueprint $table) {
+            $table->engine = 'MyISAM';
+            $table->BigIncrements('id');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->integer('reservation_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('reservation_id')->references('id')->on('reservations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deleted_reservations');
+    }
+}
