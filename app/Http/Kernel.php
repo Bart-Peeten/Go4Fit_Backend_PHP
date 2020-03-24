cc50c8922b0edd140836c06f2ea59297a0fd4b45
Bart-Peeten <bart.peeten@gmail.com>
2020-03-24 14:31:19 +0100

    Change Spatie to Fruitcake to handle CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,9 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        \Spatie\Cors\Cors::class
+        \Fruitcake\Cors\HandleCors::class,
+
+//        \Spatie\Cors\Cors::class
     ];
 
     /**
