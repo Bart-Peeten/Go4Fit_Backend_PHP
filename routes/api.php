8f645dd0eb5e7b59bc7e4a51a0a09d1b0c1f3cc2
Bart-Peeten <bart.peeten@gmail.com>
2020-05-07 10:53:47 +0200

    Move back endpoints out of auth middleware till issue in frontend is solved

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
+Route::get('users', 'UsersController@index');
+Route::get('users/{id}', 'UsersController@findById');
+Route::get('userbyemail', 'UsersController@findByEmail');
+
+Route::get('reservations', 'ReservationController@index');
+Route::get('reservationNames', 'ReservationController@getReservationsByDateAndTime');
+Route::get('weekusers', 'ReservationController@getUsersForGivenWeek');
+Route::get('cancellations', 'ReservationController@getCancellationsForGivenWeek');
+Route::get('weekreservaties', 'ReservationController@getNumberOfReservationsForGivenWeek');
+Route::get('isParticipantReserved', 'ReservationController@getIsParticipantReservedForGivenWeek');
+Route::post('reservation', 'ReservationController@addNewReservation');
+Route::post('reservationwithonlyfullname', 'ReservationController@addReservationWithOnlyFullName');
+Route::delete('deletereservation', 'ReservationController@deletereservation');
+
 Route::group(['middleware' => 'auth.jwt'], function () {
     Route::get('logout', 'AuthController@logout');
-    Route::get('users', 'UsersController@index');
-    Route::get('users/{id}', 'UsersController@findById');
-    Route::get('userbyemail', 'UsersController@findByEmail');
-
     Route::get('reservations', 'ReservationController@index');
-    Route::get('reservationNames', 'ReservationController@getReservationsByDateAndTime');
-    Route::get('weekusers', 'ReservationController@getUsersForGivenWeek');
-    Route::get('cancellations', 'ReservationController@getCancellationsForGivenWeek');
-    Route::get('weekreservaties', 'ReservationController@getNumberOfReservationsForGivenWeek');
-    Route::get('isParticipantReserved', 'ReservationController@getIsParticipantReservedForGivenWeek');
-    Route::post('reservation', 'ReservationController@addNewReservation');
-    Route::post('reservationwithonlyfullname', 'ReservationController@addReservationWithOnlyFullName');
-    Route::delete('deletereservation', 'ReservationController@deletereservation');
 });
 
 
